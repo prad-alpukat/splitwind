2fb2a1692b684f19962a4f2ad46d4aae64ce7d3a
praditya aldi syahputra <pradityaaldi.s@gmail.com>
2024-07-24 00:00:43 +0700

    add monaco editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
+import Editor from "@monaco-editor/react";
 
 export default function Home() {
   const [inputCode, setInputCode] = useState("");
   const [outputCode, setOutputCode] = useState("");
 
-  function removeDarkStateClasses(classString: string) {
+  function removeDarkStateClasses(classString: string | undefined) {
+    classString = classString ? classString : "";
     const element = new DOMParser().parseFromString(classString, "text/html");
     const the_element = element.body.firstChild as HTMLElement;
     let classList = the_element.className.split(" ");
@@ -15,49 +17,55 @@ export default function Home() {
     return the_element.outerHTML;
   }
 
-  const handleSplit = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const value = e.target.value;
-    setInputCode(value);
+  const handleSplit = (e: string | undefined) => {
+    e = e ? e : "";
+    setInputCode(e);
 
-    const data = removeDarkStateClasses(value);
+    const data = removeDarkStateClasses(e);
     setOutputCode(data);
   };
 
   return (
     <main className="container py-20">
-      <h1 className="text-3xl font-bold mb-4 text-center border-b-2 pb-4">SplitWind</h1>
-
-      <form action="">
-        <div className="grid grid-cols-2 gap-8">
-          {/* input */}
-          <div>
-            <label className="mb-2 inline-block" htmlFor="input_code">
-              Input:
-            </label>
-            <Textarea
-              id="input_code"
-              name="input_code"
-              placeholder="input Code"
-              onInput={handleSplit}
-              value={inputCode}
-            />
-          </div>
-
-          {/* output */}
-          <div>
-            <label className="mb-2 inline-block" htmlFor="output_code">
-              Output:
-            </label>
-            <Textarea
-              id="output_code"
-              name="output_code"
-              placeholder="output Code"
-              value={outputCode}
-              readOnly
-            />
-          </div>
+      <div className="border-b-2 pb-3 text-center mb-8">
+        <h1 className="text-5xl font-bold mb-4 text-center">SplitWind</h1>
+        <p>tool to remove class `dark variant` tailwindcss</p>
+      </div>
+      <div className="grid grid-cols-2 gap-8">
+        {/* input */}
+        <div>
+          <label className="mb-2 inline-block" htmlFor="input_code">
+            Input:
+          </label>
+          <Editor
+            height="90vh"
+            language="html"
+            value={inputCode}
+            onChange={handleSplit}
+            options={{
+              wordWrap: "on",
+            }}
+            className="border"
+          />
         </div>
-      </form>
+
+        {/* output */}
+        <div>
+          <label className="mb-2 inline-block" htmlFor="output_code">
+            Output:
+          </label>
+          <Editor
+            height="90vh"
+            language="html"
+            value={outputCode}
+            options={{
+              readOnly: true,
+              wordWrap: "on",
+            }}
+            className="border"
+          />
+        </div>
+      </div>
     </main>
   );
 }
